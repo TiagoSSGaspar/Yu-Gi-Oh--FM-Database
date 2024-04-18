5d36191a251f9d7b30b1d6c301c842341d4b036c
Tiago S S Gaspar <tiago.sousasilvagaspar@gmail.com>
2024-04-18 14:11:43 -0300

    add info card

--- public/js/getCardSearch.js
+++ public/js/getCardSearch.js
@@ -33,6 +33,12 @@ function createSideCard(card) {
       <div class="relative h-72 w-full overflow-hidden rounded-lg">
         <img src="public/images/cards/${card.Id}.png"  alt="${card.Name}" class="h-full w-full object-cover object-center hover:scale-105">
       </div>
+      <div class="relative mt-4">
+          <h3 class="text-sm font-medium text-gray-900">password</h3>
+          <p class="mt-1 text-sm text-gray-500">${card.CardCode}</p>
+          <h3 class="text-sm font-medium text-gray-900">Stars</h3>
+          <p class="mt-1 text-sm text-gray-500">${card.Stars}</p>
+      </div>
       <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4 border">
         <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"></div>
         <p class="relative text-lg font-semibold text-white">${card.Id}</p>
